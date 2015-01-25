dd7664be2839620413ae786ff39a92e54c48b93d
N <niko.karajannis@gmx.de>
2015-01-25 23:52:16 +0100

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates an environment with two variables and four functions; these build a parent environment for the second function
 
-## Write a short comment describing this function
+## get is the function that gives you x, the original matrix; setm is the function that creates the inverse of m; getm is the function that takes the cached inverse of the matrix; in the end, all the four functions are sored in a list
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x   <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setm <- function (solve) m <<- solve
+    getm <- function() m
+    list(set = set, get = get,
+         setm = setm,
+         getm = getm)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve-function uses the functions created in makeCacheMatrix, that is it uses and assigns the values from that environment. If the two matrices are not identical and m is not null, it takes the value from the cache; if not, it takes the inverse of m and gives it as an result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getm()
+    if (!is.null(m) && !identical(x, m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data,...)
+    x$setm(m)
+    m
 }
